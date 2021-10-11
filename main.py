7f5d46c939a3a8347ef6444346f795adc33ea315
fshakrun <82056292+fshakrun@users.noreply.github.com>
2021-10-11 23:10:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,6 @@ class VkApi:
                 if size['type'] == chart:
                     return size
 
-    # здесь должен быть токен и версия приложения vk
-    def __init__(self):
-        self.token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-        self.version = '5.124'
 
     def get_pictures(self, uid, qty):
         get_url = urljoin(self.BASE_URL, 'photos.get')
